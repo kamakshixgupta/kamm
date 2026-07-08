e88d20aad1a868e21630d68c84e240630306dc1d
aditya gupta <gupta.kamakshi452@gmail.com>
2026-07-08 11:51:32 +0530

    i am kamakshi

---
+++ OneDrive/Desktop/python.py
@@ -0,0 +1,10 @@
+# Simple Python Program
+
+name = input("Enter your name: ")
+print("Hello,", name + "!")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
